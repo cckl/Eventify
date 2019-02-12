906e64db6391b2c4c8170b6e81bcf57e9da63d69
vagrant <vagrant@vagrant.vm>
2019-02-12 22:54:37 +0000

    Add authorization with token

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, session
+from flask import Flask, flash, render_template, redirect, session
 # from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
 
@@ -10,6 +10,7 @@ app = Flask(__name__)
 app.secret_key = 'demuja'
 app.jinja_env.undefined = StrictUndefined
 
+
 @app.route('/')
 def show_homepage():
     """Displays homepage."""
@@ -18,10 +19,36 @@ def show_homepage():
 
     return render_template("homepage.html",  spotify_auth_url=spotify_auth_url)
 
+
 @app.route('/spotify-auth')
 def authorize_spotify():
 
-     return render_template("test.html")
+    response = spotify.get_auth_token()
+    print('THIS IS THE RESPONSE')
+    print(response)
+    print('\n\n\n')
+
+
+    flash("Succesfully logged into Spotify!")
+    session['spotify_token'] = response['access_token']
+    print('THIS IS THE SESSION')
+    print(session)
+    print('\n\n\n')
+
+
+    return redirect("/top-40")
+
+
+@app.route('/top-40')
+def show_top_40():
+
+    access_token = session['spotify_token']
+
+    top_artists = spotify.get_top_artists(access_token)
+
+    return render_template("test.html", artists=top_artists)
+
+
 
 if __name__ == "__main__":
     app.debug = True
--- spotify.py
+++ spotify.py
@@ -4,17 +4,20 @@ import os
 import base64
 
 from flask import request
+import requests
 
 SPOTIFY_AUTHORIZATION_URL='https://accounts.spotify.com/authorize'
 SPOTIFY_REDIRECT_URI='http://localhost:5000/spotify-auth'
 SPOTIFY_CLIENT_ID=os.environ['SPOTIFY_CLIENT_ID']
-SPOTIFY_CLIENT_SECRET=os.environ['SPOTIPY_CLIENT_SECRET']
+SPOTIFY_CLIENT_SECRET=os.environ['SPOTIFY_CLIENT_SECRET']
 SPOTIFY_TOKEN_ENDPOINT='https://accounts.spotify.com/api/token'
 
 
 def get_auth_url():
     """Generates Spotify authorization URL."""
 
+    # need auth url to login and generate authorization code
+    # specify credentials so spotify api authorizes the get request
     credentials = {
         'client_id': SPOTIFY_CLIENT_ID,
         'response_type': 'code',
@@ -30,9 +33,8 @@ def get_auth_url():
 def get_auth_token():
     """Exchange Spotify authorization code with access token."""
 
+    # gets the value of 'code' from the query string in the auth url
     auth_code = request.args.get('code')
-    # auth_code = request.args['code']
-    print(auth_code)
 
     request_params = {
         'grant_type': 'authorization_code',
@@ -40,12 +42,54 @@ def get_auth_token():
         'redirect_uri': SPOTIFY_REDIRECT_URI
     }
 
+    # https://github.com/yspark90/UniMuse/blob/master/UniMuse/spotify.py
+    # https://docs.python.org/2/library/base64.html
+    # need to import base64 to use encode and decode methods
+    # the header, as specified in the docs, needs to be a base64 encoded string that has
+    # the client ID and secret key. can use base64 to encode and decode
     client_encoded_str = base64.b64encode(f"{SPOTIFY_CLIENT_ID}:{SPOTIFY_CLIENT_SECRET}".encode('ascii'))
+    print('THIS IS ENCODED STRING')
     print(client_encoded_str)
+    print('\n\n\n')
 
-    header = {"Authorization": f"Basic {client_encoded_str.decode('ascii')}"}
-    print(header)
+
+    headers = {"Authorization": f"Basic {client_encoded_str.decode('ascii')}"}
+    print('THIS IS THE HEADER')
+    print(headers)
+    print('\n\n\n')
+
+
+    # http://flask.pocoo.org/docs/1.0/reqcontext/
+    # http://docs.python-requests.org/en/master/user/quickstart/
+    # https://www.pluralsight.com/guides/web-scraping-with-request-python
+    # need to submit auth code to spotify's token endpoint to get access token in return
+    # requests.post takes the endpoint, the request parameters as a dictionary, and header
 
     response = requests.post(SPOTIFY_TOKEN_ENDPOINT, data=request_params, headers=headers)
+
+    # need to convert to json so i can access info more easily
+    return response.json()
+
+
+def get_top_artists(access_token):
+    """Get a user's top 40 artists."""
+
+    # https://www.dataquest.io/blog/python-api-tutorial/
+    SPOTIFY_TOP_ARTISTS_ENDPOINT = 'https://api.spotify.com/v1/me/top/artists'
+
+    # necessary query params for playlist endpoint
+    query_params = {
+        'time_range': 'medium_term',
+        'limit': '40'
+    }
+
+    url = SPOTIFY_TOP_ARTISTS_ENDPOINT + "?time_range=" + query_params['time_range'] + "&limit=" + query_params['limit']
+    print('THIS IS THE URL')
+    print(url)
+    print('\n\n\n')
+
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = requests.get(url, headers=headers)
 
     return response.json()
