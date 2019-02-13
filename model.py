50dc8ae5b772e0538c7b76e4fb3aff063a3ba177
vagrant <vagrant@vagrant.vm>
2019-02-13 21:39:31 +0000

    Add event model definitions

--- model.py
+++ model.py
@@ -61,3 +61,43 @@ class UserTopArtist(db.Model):
 
     user = db.relationship('User', backref='users_top_artists')
     artist = db.relationship('Artist', backref='users_top_artists')
+
+    def __repr__(self):
+        """Provide representation of class attributes when printed."""
+
+        return f"<UserTopArtist: query_id={self.query_id},  user_id={self.user_id}, artist_id={self.artist_id}>"
+
+
+class Event(db.Model):
+    """Event from Eventbrite."""
+
+    __tablename__ = 'events'
+
+    event_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    event_name = db.Column(db.String(250), nullable=False)
+    event_date = db.Column(db.DateTime)
+    city = db.Column(db.String(50))
+    eventbrite_url = db.Column(db.String(250))
+
+    def __repr__(self):
+        """Provide representation of class attributes when printed."""
+
+        return f"<Event: event_id={self.event_id},  event_name={self.event_name}, date={self.event_date}, city={self.city}, url={elf.eventbrite_url}>"
+
+
+class UserLikedEvent(db.Model):
+    """Users' liked events from Eventbrite."""
+
+    __tablename__ = 'users_liked_events'
+
+    like_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), primary_key)
+    event_id = db.Column(db.Integer, db.ForeignKey('events.event_id'))
+
+    user = db.relationship('User', backref='users_liked_events')
+    event = db.relationship('Event', backref='users_liked_events')
+
+    def __repr__(self):
+        """Provide representation of class attributes when printed."""
+
+        return f"<UserLikedEvent: like_id={self.like_id},  user_id={self.user_id}, event_id={self.event_id}>"
