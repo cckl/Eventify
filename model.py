3fab34dce50eb724fa039e6b452360b5492cd99e
vagrant <vagrant@vagrant.vm>
2019-02-14 00:31:15 +0000

    Add backrefs

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 """Models and database functions for project."""
 
-from flask import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
@@ -20,7 +20,9 @@ class User(db.Model):
 
     # https://www.michaelcho.me/article/many-to-many-relationships-in-sqlalchemy-models-flask
     # create relationship with Artist class and association table of users' top artists
-    artists = db.relationship('Artist', secondary='users_top_artists')
+
+    # don't need the following line if i have a backref in the artist class
+    # artists = db.relationship('Artist', secondary='users_top_artists')
 
     def __repr__(self):
         """Provide representation of class attributes when printed."""
@@ -35,10 +37,11 @@ class Artist(db.Model):
 
     artist_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     artist_name = db.Column(db.String(75), nullable=False)
-    spotify_url = db.Column(db.String(150), nullable=False))
-    img = db.Column(db.String(150))
+    spotify_url = db.Column(db.String(150), nullable=False)
+    img = db.Column(db.String(150), nullable=False)
 
-    users = db.relationship('User', secondary='users_top_artists')
+    # use backref to encapsulate the relationships
+    users = db.relationship('User', secondary='users_top_artists', backref='artists')
 
     def __repr__(self):
         """Provide representation of class attributes when printed."""
@@ -56,11 +59,14 @@ class UserTopArtist(db.Model):
     __tablename__ = 'users_top_artists'
 
     query_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), primary_key)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     artist_id = db.Column(db.Integer, db.ForeignKey('artists.artist_id'))
+    timestamp = db.Column(db.DateTime(timezone=True))
 
-    user = db.relationship('User', backref='users_top_artists')
-    artist = db.relationship('Artist', backref='users_top_artists')
+
+    # can eliminate the need for these with a backref in artists
+    # user = db.relationship('User', backref='users_top_artists')
+    # artist = db.relationship('Artist', backref='users_top_artists')
 
     def __repr__(self):
         """Provide representation of class attributes when printed."""
@@ -75,14 +81,16 @@ class Event(db.Model):
 
     event_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     event_name = db.Column(db.String(250), nullable=False)
-    event_date = db.Column(db.DateTime)
+    event_date = db.Column(db.DateTime(timezone=True))
     city = db.Column(db.String(50))
     eventbrite_url = db.Column(db.String(250))
+
+    user = db.relationship('User', secondary="users_liked_events", backref='events')
 
     def __repr__(self):
         """Provide representation of class attributes when printed."""
 
-        return f"<Event: event_id={self.event_id},  event_name={self.event_name}, date={self.event_date}, city={self.city}, url={elf.eventbrite_url}>"
+        return f"<Event: event_id={self.event_id},  event_name={self.event_name}, date={self.event_date}, city={self.city}, url={self.eventbrite_url}>"
 
 
 class UserLikedEvent(db.Model):
@@ -91,13 +99,33 @@ class UserLikedEvent(db.Model):
     __tablename__ = 'users_liked_events'
 
     like_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), primary_key)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     event_id = db.Column(db.Integer, db.ForeignKey('events.event_id'))
 
-    user = db.relationship('User', backref='users_liked_events')
-    event = db.relationship('Event', backref='users_liked_events')
+    # can eliminate the need for these with a backref in Event
+    # user = db.relationship('User', backref='users_liked_events')
+    # event = db.relationship('Event', backref='users_liked_events')
 
     def __repr__(self):
         """Provide representation of class attributes when printed."""
 
         return f"<UserLikedEvent: like_id={self.like_id},  user_id={self.user_id}, event_id={self.event_id}>"
+
+
+#####################################################################
+# Helper functions
+
+def connect_to_db(app):
+    """Connect the database to the Flask app."""
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///testdb'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    from server import app
+    connect_to_db(app)
+    db.create_all()
+    print("Connected to DB ◕ ◡ ◕")
