7110c395d919113ec29beac3996c5685002c7fc7
Christina Ling <clingc7@gmail.com>
2019-03-04 19:57:18 -0800

    Add login decorators

--- views.py
+++ views.py
@@ -1,7 +1,8 @@
 """Views for application."""
 
 import os
-from flask import Flask, flash, jsonify, render_template, redirect, request, session
+from functools import wraps
+from flask import g, Flask, flash, jsonify, render_template, redirect, request, session, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
 
@@ -13,41 +14,59 @@ from model import connect_to_db, db
 import spotify
 
 # TODO: Check for user login through authentication, not just sessions
-# Use Flask.g and decorators
 # TODO: Input validation for all forms
-# TODO: Add function decorators to handle user login and logout
 # http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
 
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Hi. Please login or register 👋')
+            print(session)
+            return redirect(url_for('show_homepage'))
+    return wrap
+
+
+def logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return redirect(url_for('show_top_40'))
+        else:
+            return f(*args, **kwargs)
+    return wrap
+
+
+def spotify_login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'spotify_logged_in' in session:
+            print(session)
+            return f(*args, **kwargs)
+        else:
+            flash('Hi. Please login to Spotify 🎧')
+            print(session)
+            return redirect(url_for('get_top_40'))
+    return wrap
+
+
 @app.route('/')
+@logged_in
 def show_homepage():
     """Display homepage."""
 
-    session.clear()
-
-    if 'user' in session:
-        if 'access_token' in session:
-            flash('You are already logged in to Spotify.')
-            return redirect('/top-40')
-        else:
-            flash('You are already logged in.')
-            return redirect('/get-top-40')
-    else:
-        return render_template("homepage.html")
+    return render_template("homepage.html")
 
 
 @app.route('/login')
+@logged_in
 def show_login():
     """Display user login page."""
 
-    if 'user' in session:
-        if 'access_token' in session:
-            flash('You are already logged in to Spotify.')
-            return redirect('/top-40')
-        else:
-            flash('You are already logged in.')
-            return redirect('/get-top-40')
-    else:
-        return render_template("login.html")
+    return render_template("login.html")
 
 
 @app.route('/login', methods=['POST'])
@@ -62,9 +81,8 @@ def process_login():
     if user:
         if password == user.password:
             session['user'] = username
+            session['logged_in'] = True
             flash('Successfully logged in 😸')
-            # FIXME: still redirecting to /get-top-40. maybe cuz spotify
-            # doesn't automatically login?
             return redirect('/top-40')
         else:
             flash('Sorry, that username or password isn\'t correct 😧 Try again.')
@@ -75,18 +93,11 @@ def process_login():
 
 
 @app.route('/register')
+@logged_in
 def show_registration():
     """Display user registration page."""
 
-    if 'user' in session:
-        if 'access_token' in session:
-            flash('You are already logged in to Spotify.')
-            return redirect('/top-40')
-        else:
-            flash('You are already logged in.')
-            return redirect('/get-top-40')
-    else:
-        return render_template("register.html")
+    return render_template("register.html")
 
 
 @app.route('/register', methods=['POST'])
@@ -105,83 +116,68 @@ def process_registration():
 
         # creates a user session
         session['user'] = username
+        session['logged_in'] = True
         flash('Successfully created an account. 🐙')
         return redirect('/get-top-40')
 
 
 @app.route('/logout')
+@login_required
 def logout():
     """Logout user."""
 
     session.clear()
-    print(session)
-
     return redirect("/")
 
 
 @app.route('/get-top-40')
+@login_required
 def get_top_40():
     """Display Spotify login."""
 
-    print(session)
-    if 'user' not in session:
-        flash('Please login or register 👋🏻')
-        return redirect('/')
-    if 'access_token' in session:
-        flash('You are already logged in to Spotify.')
-        return redirect('/top-40')
-    else:
-        spotify_auth_url = spotify.get_auth_url()
-        return render_template("get-top-40.html", spotify_auth_url=spotify_auth_url)
+    spotify_auth_url = spotify.get_auth_url()
+    return render_template("get-top-40.html", spotify_auth_url=spotify_auth_url)
 
 
 @app.route('/spotify-auth')
+@login_required
 def authorize_spotify():
     """Spotify authorization callback."""
 
     response = spotify.get_access_token(request)
-    flash("Succesfully logged into Spotify! 👾")
     session['access_token'] = response['access_token']
     session['refresh_token'] = response['refresh_token']
+    session['spotify_logged_in'] = True
 
+    flash("Succesfully logged into Spotify! 👾")
     return redirect("/top-40")
 
 
 @app.route('/top-40')
+@login_required
+@spotify_login_required
 def show_top_40():
     """Display user's top 40 Spotify artists."""
 
-    print(session)
-    # TODO: Add handling for when Sptify token expires
-    if 'access_token' not in session:
-        if 'user' not in session:
-            flash('Please login or register. 👋🏻')
-            return redirect('/')
-        else:
-            flash('Please login to Spotify 🎧')
-            return redirect('/get-top-40')
-    else:
-        access_token = session['access_token']
+    access_token = session['access_token']
+    user_data = spotify.get_user_profile(access_token)
+    artists = spotify.get_top_artists(access_token)
+    username = session['user']
 
-        # get user profile and artist info
-        user_data = spotify.get_user_profile(access_token)
-        artists = spotify.get_top_artists(access_token)
+    # after classes encapsulate instantiation, can create function "add user"
+    # should extract the user/artist creation process to another filter_events
+    # 'if': can this be extracted
+    if helper.check_spotify_not_in_db(username):
+        helper.add_user_spotify_db(access_token)
+        helper.add_artist_db(access_token)
+        helper.add_user_artist_link(access_token)
 
-        # add new user's info to db
-        username = session['user']
-
-        # after classes encapsulate instantiation, can create function "add user"
-        # should extract the user/artist creation process to another filter_events
-        # 'if': can this be extracted
-        if helper.check_spotify_not_in_db(username):
-            helper.add_user_spotify_db(access_token)
-            helper.add_artist_db(access_token)
-            helper.add_user_artist_link(access_token)
-
-        return render_template("top-40.html", artists=artists, user=user_data)
+    return render_template("top-40.html", artists=artists, user=user_data)
 
 
 @app.route('/event-search')
+@login_required
+@spotify_login_required
 def search_events():
     """Search for events using the Eventbrite API."""
 
@@ -220,7 +216,6 @@ def search_recommended_events():
 
     access_token = session['access_token']
     related_artists = spotify.get_related_artists(access_token)
-
     results = eventbrite.search_batch_events(related_artists, city, distance)
 
     return jsonify(results)
