2914caa62192c90e722a6b233fb64cbe22b8da19
Christina Ling <clingc7@gmail.com>
2019-03-13 19:11:37 -0700

    Add loading animation, form handling, and event res styling

--- static/scripts.js
+++ static/scripts.js
@@ -10,21 +10,27 @@ function loadingAnimationSlide() {
 }
 
 $(document).bind('ajaxSend', function() {
-  $('#loading').fadeIn(400).show();
-  $('#loading2').fadeIn(800).show();
-  loadingAnimationFade();
-  loadingAnimationSlide();
+  $('#loading-icons').css('visibility', 'visible');
+  // $('#loading2').fadeIn(400).show();
+  // loadingAnimationFade();
+  // loadingAnimationSlide();
 });
 
 $(document).bind('ajaxComplete', function() {
-  $('#loading').empty();
-  $('#loading2').empty();
+  $('#loading-icons').css('visibility', 'hidden');
+  // $('#loading2').empty();
 });
 
+function formatDate(datetime) {
+  let date = new Date(datetime)
+  date_split = date.toString().split(" ")
+  return date_split
+}
 
 // event search with user artists
 $('#event-search').on('submit', (evt) => {
   evt.preventDefault();
+  console.log('submitted')
 
   const formInputs = {
     'city': $('#city').val(),
@@ -33,20 +39,22 @@ $('#event-search').on('submit', (evt) => {
 
   $.post('/event-search', formInputs, (results) => {
     console.log(results);
-    if (results) {
-      $('#event-header').html('<h2>Events near you</h2><br>')
+    if (results.length >= 1) {
+      $('#event-header').html('<h1><b>Events near you</b></h1><br>')
       for (let result of results) {
+        formattedDate = formatDate(result['starts_at'])
         $('#event-results').append(`
-          <div class="event-div" id="orange-bg">
+          <div class="event-div" id="yellow-bg">
               <div class="rect-img img-fluid">
                 <img class="event-img" src="${result['img']}">
               </div>
               <div class="event-text">
                 <div class="row">
-                  <div class="col-4">
-                    <p>${result['starts_at']}</p>
+                  <div class="col-3 text-center">
+                    <p>${formattedDate[1]}</p>
+                    <h2>${formattedDate[2]}</h2>
                   </div>
-                  <div class="col-8">
+                  <div class="col-9">
                     <h4>${result['name']}</h4>
                     <p>${result['venue']}</p>
                     <a class="btn btn-info" href="${result['url']}" target="_blank">RSVP on Eventbrite</a>
@@ -55,17 +63,19 @@ $('#event-search').on('submit', (evt) => {
               </div>
             </div>
           `)
+          $('#search-again').show();
       }
     } else {
-      $('#none-found').show()
+      $('#none-found').show();
+      $('#search-again').show();
     }
   });
-  $('#search-area').hide();
+  $('input[type=submit]').hide();
 });
 
 
 // event search with recommended events
-$('#recommended-events').on('submit', (evt) => {
+$('#recommended').on('submit', (evt) => {
   evt.preventDefault();
   $('#none-found').hide();
 
@@ -77,19 +87,21 @@ $('#recommended-events').on('submit', (evt) => {
   $.post('/recommended', formInputs, (results) => {
     console.log(results)
     if (results) {
-      $('#event-results').append('<h2>You might like</h2>')
+      // $('#event-results').append('<h2>You might like</h2>')
       for (let result of results) {
+        formattedDate = formatDate(result['starts_at'])
         $('#event-results').append(`
-          <div class="event-div">
+          <div class="event-div" id="orange-bg">
               <div class="rect-img img-fluid">
                 <img class="event-img" src="${result['img']}">
               </div>
               <div class="event-text">
                 <div class="row">
-                  <div class="col-4">
-                    <p>${result['starts_at']}</p>
+                  <div class="col-3 text-center">
+                    <p>${formattedDate[1]}</p>
+                    <h2>${formattedDate[2]}</h2>
                   </div>
-                  <div class="col-8">
+                  <div class="col-9">
                     <h4>${result['name']}</h4>
                     <p>${result['venue']}</p>
                     <a class="btn btn-info" href="${result['url']}" target="_blank">RSVP on Eventbrite</a>
@@ -103,12 +115,15 @@ $('#recommended-events').on('submit', (evt) => {
       $('#none-found').show()
     }
   });
-  $('#search-area2').hide();
+  $('input[type=submit]').hide();
 });
 
 
 $('#search-again').on('click', () => {
-  $('#event-results').empty()
-  $('#search-area').show()
-  $('#search-area2').show()
+  $('#event-header').empty();
+  $('#event-results').empty();
+  $('input[type=submit]').show();
+  // $('#search-area').show();
+  // $('#search-area2').show();
+  $('#search-again').hide();
 })
